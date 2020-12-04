570b04b87e4a35aad138445e78cb07b0cc852915
Marcelo Ludovico de Paula <marcelo.paula@eptv.com.br>
2020-12-04 18:06:56 -0300

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by marcelo on 04/12/2020.
+//  Copyright © 2020 marcelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
