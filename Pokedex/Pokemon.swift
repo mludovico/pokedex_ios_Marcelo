83b100513a7b7107f1d7847fa26b1bb17960dc90
Marcelo Ludovico de Paula <marcelo.paula@eptv.com.br>
2020-12-04 19:59:42 -0300

    Initial commit

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,33 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by marcelo on 04/12/2020.
+//  Copyright © 2020 marcelo. All rights reserved.
+//
+
+import Foundation
+
+struct PokemonList: Codable {
+    let results: [Pokemon]
+}
+
+struct Pokemon: Codable {
+    let name: String
+    let url: String
+}
+
+struct PokemonData: Codable {
+    let id: Int
+    let type: [PokemonTypeEntry]
+}
+
+struct PokemonType: Codable {
+    let name: String
+    let url: String
+}
+
+struct PokemonTypeEntry: Codable {
+    let slot: Int
+    let type: PokemonType
+}
---
+++ Pokedex/PokemonViewController.swift
@@ -0,0 +1,56 @@
+//
+//  PokemonViewController.swift
+//  Pokedex
+//
+//  Created by marcelo on 04/12/2020.
+//  Copyright © 2020 marcelo. All rights reserved.
+//
+
+import UIKit
+
+class PokemonViewController: UIViewController {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var numberLabel: UILabel!
+    @IBOutlet var type1Label: UILabel!
+    @IBOutlet var type2Label: UILabel!
+    var pokemon: Pokemon!
+    
+    override func viewDidLoad() {
+        
+        type1Label.text = ""
+        type2Label.text = ""
+        
+        super.viewDidLoad()
+        let url = URL(string: pokemon.url)
+        guard let u = url else {
+            return
+        }
+        URLSession.shared.dataTask(with: u) {
+            (data, response, error) in
+            guard let data = data else {
+                return
+                
+            }
+            do{
+                let pokemonData = try JSONDecoder().decode(PokemonData.self, from: data)
+                DispatchQueue.main.async {
+                    
+                    self.nameLabel.text = self.pokemon.name
+                    self.numberLabel.text = String(format: "#%03d", pokemonData.id)
+                    for typeEntry in pokemonData.type {
+                        if typeEntry.slot == 1 {
+                            self.type1Label.text = typeEntry.type.name
+                        }
+                        else if typeEntry.slot == 2 {
+                            self.type2Label.text = typeEntry.type.name
+                        }                        
+                    }
+                }
+            }catch let error {
+                print("\(error)")
+            }
+            
+            }.resume()
+    }
+    
+}
